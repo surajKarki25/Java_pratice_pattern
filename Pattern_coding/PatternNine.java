b94ba580a4d3a6f45cef6e2592635b411adb339c
suraj <singhkarki160@gmail.com>
2023-07-13 18:12:14 +0530

    start pattern code

---
+++ Pattern_coding/PatternNine.java
@@ -0,0 +1,22 @@
+
+public class PatternNine{
+  public static void main(String[] args) {
+	   int n=7;
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=n-i;j++){
+               System.out.print(" ");
+              }
+           for(int k=1;k<=i;k++){
+               System.out.print("*");
+               }
+           for(int l=1;l<=i-1;l++) {
+        	   System.out.print("*");
+           }
+               
+               
+           System.out.println();
+           }
+       }
+           
+       }
+
